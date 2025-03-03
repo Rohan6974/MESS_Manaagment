f6f9f60eb36bd32b9c5df075015d40d8cc212f97
Rohan6974 <157485942+Rohan6974@users.noreply.github.com>
2025-03-03 11:29:43 +0530

    first commit ( all dependencies installed )

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <div className="App-header">
+     
+    </div>
+  )
+}
+
+export default App;
